8024dfe1b4919f0a450caa0bff40bae4dd216977
david <911dms911dms@protonmail.com>
2022-12-04 16:24:15 -0500

    string values added

---
+++ app/src/main/java/com/example/circadianclock/ui/theme/Strings.kt
@@ -0,0 +1,32 @@
+package com.example.circadianclock
+
+//MainScreen
+val time = "XX:XX"
+val month = "November"
+val dayOfMonth = "15"
+val year = "2022"
+val length = "Length"
+val nightLength = "10:48"
+val dayLength = "12:12"
+
+//SettingsScreen
+val location = "Location"
+val timeType = "Time Type"
+val dateType = "Date Type"
+val longitude = "longitude"
+val latitude = "latitude"
+val UTC = "UTC"
+val EST = "EST"
+val LT = "LT"
+val gregorian = "Gregorian"
+val siwarski = "Siwarski"
+
+//Day and Night Length
+val day = "day"
+val night = "night"
+val N = "1N"
+val D = "1D"
+val W = "1W"
+val M = "1M"
+val MMM = "3M"
+val Y = "1Y"
